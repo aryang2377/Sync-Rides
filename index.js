f9072ee54944b03d4fd3d9fe63e9373b0ebaea2e
krishbudhia <krishbudhia123@gmail.com>
2026-03-19 20:05:22 +0530

    new commit

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express')
+const express = require("express");
+const path = require("path");
+const method = require("method-override");
+const ejsMate = require("ejs-mate");
+const app = express();
 
-const app = express()
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(method("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-  res.render("./")
-})
+app.get("/", (req, res) => {
+  res.render("listings/dashboard");
+});
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
-})
+  console.log("Server is running on http://localhost:3000");
+});
