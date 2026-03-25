2feca7a47a5ee87261e4d7fdc2c7e4376a734818
aryang2377 <aryanq102@gmail.com>
2026-03-25 14:08:37 +0530

    database linking

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const path = require("path");
-const method = require("method-override");
-const ejsMate = require("ejs-mate");
-const app = express();
+import express from "express"
+import path from "path"
+import method from "method-override"
+import ejsMate from "ejs-mate"
+import dotenv from "dotenv"
+import connectDB from "./db/sample.js";
 
+const app = express();
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(process.cwd(), "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(method("_method"));
 app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(process.cwd(), "public")));
 
 app.use((req, res, next) => {
   res.locals.currentPath = req.path;
@@ -28,6 +30,20 @@ app.get("/login", (req, res) => {
 app.get("/signup", (req, res) => {
   res.render("users/signup");
 });
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+// app.listen(3000, () => {
+//   console.log("Server is running on http://localhost:3000");
+// });
+
+dotenv.config({
+  path: "./.env"
 });
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running at port:$ {process.env.PORT}`);
+    })
+  })
+  .catch((err) => {
+    console.log("MONGO Db connection failed", err);
+  })
