fab3abb995225a60f95a7f0e05bb4dff59d87402
aryang2377 <aryanq102@gmail.com>
2026-04-05 20:14:12 +0530

    rider_booking

--- models/trip.js
+++ models/trip.js
@@ -1,34 +1,49 @@
 import mongoose from "mongoose";
-const tripSchema=new mongoose.Schema({
-        driver:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"user",
-            required:true
-        },
-        pickup:{
-            type:String,
-            required:true
-        },
-        destination:{
-            type:String,
-            required:true
-        },
-        time:{
-            type:Date,
-            required:true
-        },
-        Seats:{
-            type:Number,
-            default:1,
-            required:true
-        },
-        passengers:[{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"user"
+
+const tripSchema = new mongoose.Schema({
+
+    driver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    pickup: {
+        type: String,
+        required: true
+    },
+
+    destination: {
+        type: String,
+        required: true
+    },
+
+    time: {
+        type: Date,
+        required: true
+    },
+
+    seats: {
+        type: Number,
+        default: 1,
+        required: true
+    },
+
+    status: {
+        type: String,
+        enum: ["scheduled", "completed", "cancelled"],
+        default: "scheduled"
+    },
+
+    passengers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user"
         }
     ]
-    },
-);
+
+});
 
 const Trip = mongoose.model("Trip", tripSchema);
+
 export default Trip;
